7485c180db8dbf2c03d1c1ab67a3cbf650195843
WylieMabel <s2020073@ed.ac.uk>
2023-11-28 18:30:47 +0000

    Fixed output

--- Model Output/modelScript.py
+++ Model Output/modelScript.py
@@ -77,82 +77,89 @@ def convertHydrologyToDF(hydrologyArray, data):
     hydrologyData["yield"] = hydrologyArray.reshape((2601,1))
     return hydrologyData
 
-def fullModelRun(climate, leadFarmers, social, input_csv_path, no_of_years):
-    # sets up model
-    netlogo = setUpNetLogoModel(leadFarmers, social[0], social[1], social[2])
+def fullModelRun(paramArray, input_csv_path, no_of_years):
+    for paramIndex in range(0,18):
+        # sets up model
+        climate = paramArray[paramIndex][0]
+        leadFarmers = paramArray[paramIndex][1]
+        social = paramArray[paramIndex][2]
 
-    # this will record all field attributes throughout the simulation - need to add year index to differentiate
-    baseFieldData = reportsToDataFrame(netlogo)
+        netlogo = setUpNetLogoModel(leadFarmers, social[0], social[1], social[2])
 
-    returnedData = baseFieldData.copy()
-    
-    baseFieldData["Year"] = 0
+        # this will record all field attributes throughout the simulation - need to add year index to differentiate
+        baseFieldData = reportsToDataFrame(netlogo)
 
-    WSA_records = []
-
-    
-    #get input temperature data
-    avg, maxi, mini = get_yearly_temp(input_csv_path, no_of_years)
-    avg = np.array(avg)
-    maxi = np.array(maxi)
-    mini = np.array(mini)
-
-    Ecohyd_model = EcoHyd(climate, 20, 26, 23)
-
-    #--------------------------------------------#
-    # let hydrology model spin up for five years #
-    for i in range(0,5):
-        #just use same initial WSA array for each year
-        WSA_array = convertWSAToNPArray(returnedData)
-        _,_ = Ecohyd_model.stepper(WSA_array, avg[0]+climate['tempshift'], maxi[0]+climate['tempshift'], mini[0]+climate['tempshift'])
-
-    #---------------------------------#
-    # actual coupled model loop whooo #
-    for year in range(0, no_of_years):
-        print("year:", year)
-
-        # converts the usingWSA bool for each field into an NP array
-        WSA_array = convertWSAToNPArray(returnedData)
-        WSA_records.append([WSA_array])
-
-        biomass_harvest, SM_canic_end = Ecohyd_model.stepper(WSA_array, avg[year]+climate['tempshift'], maxi[year]+climate['tempshift'], mini[year]+climate['tempshift'])
-
-        #record outputs for yearly rainfall
-        cum_rainfall = np.cumsum(Ecohyd_model.rain_tseries[(year)*365:(year+1)*365])[-1]
-
-        returnedData.sort_values(by=['ycor', 'xcor'], ascending=[False, True], inplace=True)
-
-        # converts the updated yields to the dataframe
-        hydrologyData = convertHydrologyToDF(biomass_harvest,returnedData)
+        returnedData = baseFieldData.copy()
         
-        # writes this new yield information to the netlogo implementation
-        netlogo.write_NetLogo_attriblist(hydrologyData, "field")
+        baseFieldData["Year"] = 0
+        baseFieldData["TotalYearRainfall"] = 0
 
-        # runs one step of social model
-        netlogo.command("farming-year")
+        WSA_records = []
 
-        # converts field data to a df
-        returnedData = reportsToDataFrame(netlogo)
-
-        dataToRecord = returnedData.copy()
         
-        dataToRecord["Year"] = year + 1
+        #get input temperature data
+        avg, maxi, mini = get_yearly_temp(input_csv_path, no_of_years)
+        avg = np.array(avg)
+        maxi = np.array(maxi)
+        mini = np.array(mini)
 
-        dataToRecord["TotalYearRainfall"] = cum_rainfall
+        Ecohyd_model = EcoHyd(climate, 20, 26, 23)
 
-        # adds this years results to the dataframe
-        baseFieldData = pd.concat([baseFieldData, dataToRecord], ignore_index=True)
+        #--------------------------------------------#
+        # let hydrology model spin up for five years #
+        for i in range(0,5):
+            #just use same initial WSA array for each year
+            WSA_array = convertWSAToNPArray(returnedData)
+            _,_ = Ecohyd_model.stepper(WSA_array, avg[0]+climate['tempshift'], maxi[0]+climate['tempshift'], mini[0]+climate['tempshift'])
 
-    summarisedData = baseFieldData.groupby(["owner-id","Year"]).agg({'xcor':'mean','ycor':'mean','implements-WSA':'mean','owner-knows-WSA':'mean', 'yield':'sum', 'who':'count'}).reset_index()
-    summarisedData.rename(columns={'owner-id':'FarmerID', 'xcor':'MeanXCor', 'ycor':'MeanYCor', 'implements-WSA':'ImplementingWSA','owner-knows-WSA':'KnowsWSA','yield':'TotalYield','who':'NumberofFields'})
+        #---------------------------------#
+        # actual coupled model loop whooo #
+        for year in range(0, no_of_years):
+            print("year:", year)
 
-    summarisedData["LeadFarmers"] = leadFarmers
-    summarisedData["SocialScenario"] = social[3]
-    if climate['mean_raindpth_wet'] == 10:
-        summarisedData["ClimateScenario"] = "Current Climate"
-    else:
-        summarisedData["ClimateScenario"] = "Warm Climate"
-    summarisedData["UniqueID"] = str(datetime.datetime.now())
-    
-    # this writes to a csv
-    summarisedData.to_csv(path_or_buf="sampleModelOutput", mode = "a", index=False, header = True)
+            # converts the usingWSA bool for each field into an NP array
+            WSA_array = convertWSAToNPArray(returnedData)
+            WSA_records.append([WSA_array])
+
+            biomass_harvest, SM_canic_end = Ecohyd_model.stepper(WSA_array, avg[year]+climate['tempshift'], maxi[year]+climate['tempshift'], mini[year]+climate['tempshift'])
+
+            #record outputs for yearly rainfall
+            cum_rainfall = np.cumsum(Ecohyd_model.rain_tseries[(year)*365:(year+1)*365])[-1]
+
+            returnedData.sort_values(by=['ycor', 'xcor'], ascending=[False, True], inplace=True)
+
+            # converts the updated yields to the dataframe
+            hydrologyData = convertHydrologyToDF(biomass_harvest,returnedData)
+            
+            # writes this new yield information to the netlogo implementation
+            netlogo.write_NetLogo_attriblist(hydrologyData, "field")
+
+            # runs one step of social model
+            netlogo.command("farming-year")
+
+            # converts field data to a df
+            returnedData = reportsToDataFrame(netlogo)
+
+            dataToRecord = returnedData.copy()
+            
+            dataToRecord["Year"] = year + 1
+
+            dataToRecord["TotalYearRainfall"] = cum_rainfall
+
+            # adds this years results to the dataframe
+            baseFieldData = pd.concat([baseFieldData, dataToRecord], ignore_index=True)
+
+        summarisedData = baseFieldData.groupby(["owner-id","Year"]).agg({'xcor':'mean','ycor':'mean','implements-WSA':'mean','owner-knows-WSA':'mean', 'yield':'sum', 'TotalYearRainfall':'mean', 'who':'count'}).reset_index()
+        summarisedData.rename(columns={'owner-id':'FarmerID', 'xcor':'MeanXCor', 'ycor':'MeanYCor', 'implements-WSA':'ImplementingWSA','owner-knows-WSA':'KnowsWSA','yield':'TotalYield','who':'NumberofFields'})
+
+        summarisedData["LeadFarmers"] = leadFarmers
+        summarisedData["SocialScenario"] = social[3]
+        if climate['mean_raindpth_wet'] == 10:
+            summarisedData["ClimateScenario"] = "Current Climate"
+        else:
+            summarisedData["ClimateScenario"] = "Warm Climate"
+        summarisedData["UniqueID"] = str(datetime.datetime.now())
+        
+        fileName = "modelOutputParamCombo" + str(paramIndex)
+        # this writes to a csv
+        summarisedData.to_csv(path_or_buf=fileName, mode = "a", index=False, header = True)
